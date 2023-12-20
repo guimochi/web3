1612b0b2964b47290198ccef59b9dee261adccc9
guimochi <yamamoto.guilherme@outlook.com>
2023-12-20 15:53:06 +0100

    module2 : init

--- exercises/module2/src/components/App/App.jsx
+++ exercises/module2/src/components/App/App.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
+import Display from 'components/Display/Display';
+import Button from 'components/Button/Button';
+
 const App = () => {
 	const [counter, setCounter] = useState(0);
+	console.log('🚀 ~ file: App.jsx:7 ~ App ~ counter:', counter);
 
-	const increaseByOne = () => setCounter(counter + 1);
+	const increaseByOne = () => {
+		console.log('increasing, value before', counter);
+		setCounter(counter + 1);
+	};
 
-	const setToZero = () => setCounter(0);
+	const decreaseByOne = () => {
+		console.log('decreasing, value before', counter);
+		setCounter(counter - 1);
+	};
+
+	const setToZero = () => {
+		console.log('resetting to zero, value before', counter);
+		setCounter(0);
+	};
 
 	return (
 		<>
 			<Display counter={counter} />
-			<button onClick={increaseByOne}>plus</button>
-			<button onClick={setToZero}>zero</button>
+			<Button onClick={increaseByOne} text='plus' />
+			<Button onClick={setToZero} text='zero' />
+			<Button onClick={decreaseByOne} text='minus' />
 		</>
 	);
 };
