63d234d03a447aa612fca387c5a17dca766daa94
guimochi <yamamoto.guilherme@outlook.com>
2023-12-21 10:44:29 +0100

    module2 : custom hook

--- exercises/module2/src/components/App/App.jsx
+++ exercises/module2/src/components/App/App.jsx
@@ -1,23 +1,16 @@
-import { useState } from 'react';
 import Display from 'components/Display/Display';
 import Button from 'components/Button/Button';
+import useLocalStorage from 'hooks/useLocalStorage';
 
 const App = () => {
-	const [counter, setCounter] = useState(
-		JSON.parse(localStorage.getItem('counter'))
-	);
-
-	const changeCount = (delta) => {
-		setCounter(counter + parseInt(delta));
-		localStorage.setItem('counter', JSON.stringify(counter));
-	};
+	const [counter, setCounter] = useLocalStorage('counter', 0);
 
 	return (
 		<>
 			<Display counter={counter} />
-			<Button changeCount={changeCount} delta={1} text='plus' />
-			<Button delta={-counter} changeCount={changeCount} text='reset' />
-			<Button delta={-1} changeCount={changeCount} text='minus' />
+			<Button changeCount={setCounter} delta={1} text='plus' />
+			<Button delta={-counter} changeCount={setCounter} text='reset' />
+			<Button delta={-1} changeCount={setCounter} text='minus' />
 		</>
 	);
 };
